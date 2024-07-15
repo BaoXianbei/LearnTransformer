1130242d3d584f4ad9cc71707ed5e54a17769cb6
Kris <kk07213115@outlook.com>
2024-07-15 16:36:27 +0800

    test

--- DataProcess.py
+++ DataProcess.py
@@ -1,4 +1,19 @@
 import torch
-import numpy
-import pandas
+from swin_transformer_pytorch import SwinTransformer
 
+net = SwinTransformer(
+    hidden_dim=96,
+    layers=(2, 2, 6, 2),
+    heads=(3, 6, 12, 24),
+    channels=3,
+    num_classes=3,
+    head_dim=32,
+    window_size=7,
+    downscaling_factors=(4, 2, 2, 2),
+    relative_pos_embedding=True)
+
+dummy_x = torch.randn(1, 3, 224, 224)
+logits = net(dummy_x)  # (1,3)
+
+print(net)
+print(logits)
